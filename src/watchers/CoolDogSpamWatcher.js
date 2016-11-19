fc6ea37552448148f678f4c8ea684b8f7fe6d73b
Ryan Dowling <ryan@ryandowling.me>
2016-11-19 12:28:05 +1100

    Add in CoolDogSpamWatcher

---
+++ src/watchers/CoolDogSpamWatcher.js
@@ -0,0 +1,43 @@
+import BaseWatcher from './BaseWatcher';
+
+import config from '../config';
+
+/**
+ * This checks for people spamming cool dog crap.
+ */
+class CoolDogSpamWatcher extends BaseWatcher {
+    constructor(bot) {
+        super(bot);
+    }
+
+    usesBypassRules = true;
+
+    /**
+     * The method this watcher should listen on.
+     *
+     * @type {string}
+     */
+    method = [
+        'message',
+        'messageUpdate'
+    ];
+
+    async action(method, message, updatedMessage) {
+        if (method === 'messageUpdate') {
+            message = updatedMessage;
+        }
+
+        const rulesChannel = this.bot.channels.find((channel) => (channel.name === config.rules_channel));
+
+        if (message.cleanContent.toLowerCase().indexOf('this is cooldog') !== -1) {
+            const warningMessage = await message.reply(`Please read the ${rulesChannel} channel. Spamming or encouraging spamming is not allowed.`);
+
+            this.addWarningToUser(message);
+
+            message.delete();
+            warningMessage.delete(60000);
+        }
+    }
+}
+
+export default CoolDogSpamWatcher;
