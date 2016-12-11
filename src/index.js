118f18e509084522d4a47abc5e13f87c915705c7
Ryan Dowling <ryan@ryandowling.me>
2016-12-11 20:08:06 +1100

    Add in exception handlers

--- src/index.js
+++ src/index.js
@@ -5,3 +5,11 @@ import Bot from './Bot';
 const bot = new Bot();
 
 bot.start();
+
+process.on('uncaughtException', function (error) {
+    console.error('uncaughtException', error || '');
+});
+
+process.on('unhandledRejection', function (reason, p) {
+    console.error('unhandledRejection', reason || '', p || '');
+});
