85faf5da900037d2db54c6d9e9e6d784a40d22dc
Ryan Dowling <ryan@ryandowling.me>
2016-12-24 13:05:02 +1100

    Add in a watcher for managing spam

---
+++ src/watchers/LinkSpamWatcher.js
@@ -0,0 +1,39 @@
+import BaseWatcher from './BaseWatcher';
+
+/**
+ * This checks for people spamming links.
+ */
+class LinkSpamWatcher extends BaseWatcher {
+    constructor(bot) {
+        super(bot);
+    }
+
+    usesBypassRules = true;
+
+    /**
+     * The method this watcher should listen on.
+     *
+     * @type {string}
+     */
+    method = [
+        'message',
+        'messageUpdate'
+    ];
+
+    async action(method, message, updatedMessage) {
+        if (method === 'messageUpdate') {
+            message = updatedMessage;
+        }
+
+        if (message.cleanContent.toLowerCase().indexOf('giftsofsteam.com') !== -1) {
+            const warningMessage = await message.reply(`This link is not allowed to be posted as it is a known hoax/spam/scam.`);
+
+            this.addWarningToUser(message);
+
+            message.delete();
+            warningMessage.delete(60000);
+        }
+    }
+}
+
+export default LinkSpamWatcher;
