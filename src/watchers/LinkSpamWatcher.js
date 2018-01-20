29c84992559879e5ee7122b458ba2d0e3d4e91a0
Ryan Dowling <RyanTheAllmighty@users.noreply.github.com>
2018-01-20 20:36:35 +1100

    Update LinkSpamWatcher.js

--- src/watchers/LinkSpamWatcher.js
+++ src/watchers/LinkSpamWatcher.js
@@ -55,7 +55,8 @@ class LinkSpamWatcher extends BaseWatcher {
             cleanMessage.includes('link.clashroyale.com') ||
             cleanMessage.includes('paysafecards.org') ||
             cleanMessage.includes('minecraftgeek.com') ||
-            cleanMessage.includes('?ref=')
+            cleanMessage.includes('?ref=') ||
+            cleanMessage.includes('free-gg.com')
         ) {
             const warningMessage = await messageToActUpon.reply(
                 `This link is not allowed to be posted as it is a known hoax/spam/scam.`
