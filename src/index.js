9a680020398f1f9f0b4f79035c114802f2ac41d4
Ryan Dowling <ryan@ryandowling.me>
2018-11-14 09:48:46 +1100

    fix: exit process on uncaughtException or unhandledRejection

--- src/index.js
+++ src/index.js
@@ -7,9 +7,13 @@ bot.start();
 process.on('uncaughtException', (error) => {
     // eslint-disable-next-line no-console
     console.error('uncaughtException', error || '');
+    
+    process.exit(1);
 });
 
 process.on('unhandledRejection', (reason, p) => {
     // eslint-disable-next-line no-console
     console.error('unhandledRejection', reason || '', p || '');
+    
+    process.exit(1);
 });
