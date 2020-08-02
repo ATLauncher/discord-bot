6da54a93c6670cf63420a328ed477bc079519dd5
Ryan Dowling <ryan@ryandowling.me>
2020-08-02 13:09:20 +1000

    fix: make cyt command nicer

--- src/commands/FreeSupportCommand.ts
+++ src/commands/FreeSupportCommand.ts
@@ -5,16 +5,13 @@ import { PERMISSIONS } from '../constants/discord';
 
 /**
  * Simple command to remind people support is free and not immediate.
- *
- * @class CalmYourTitsCommand
- * @extends {BaseCommand}
  */
-class CalmYourTitsCommand extends BaseCommand {
+class FreeSupportCommand extends BaseCommand {
     /**
      * The pattern to match against. If the message matches this pattern then we will respond to it with the action
      * method.
      */
-    pattern = /^!cyt/;
+    pattern = /^!freesupport/;
 
     /**
      * The permissions the user requires in order to use this command.
@@ -25,21 +22,14 @@ class CalmYourTitsCommand extends BaseCommand {
      * The function that should be called when the event is fired.
      */
     async execute(message: Discord.Message) {
-        message.channel.send(
+        await message.channel.send(
             [
-                '⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔',
-                '',
                 'Everyone in here are volunteers who take time out of their day to help you.',
-                'They do not have to help you; it is their choice. Respect this, and be patient when awaiting ' +
+                "They do not have to help you; it's their choice. Respect this, and please be patient when awaiting " +
                     'help, or they may decide not to help you.',
                 '',
-                "If you don't feel like waiting you are perfectly welcome to Google your problem and attempt to " +
+                "If you don't feel like waiting, you're perfectly welcome to Google your problem and attempt to " +
                     'fix it yourself.',
-                '',
-                "If you tried to get this level of support elsewhere, you'd be paying over $50(USD)/hr. Be " +
-                    'grateful.',
-                '',
-                '⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔~~⛔',
             ].join('\n'),
         );
 
@@ -47,4 +37,4 @@ class CalmYourTitsCommand extends BaseCommand {
     }
 }
 
-export default CalmYourTitsCommand;
+export default FreeSupportCommand;
