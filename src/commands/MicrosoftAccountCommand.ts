1a887928eaf71498daee2bbd488ab1a2d2252810
Ryan Dowling <ryan@ryandowling.me>
2020-12-07 15:18:22 +1100

    chore: update text on !microsoftaccount command

--- src/commands/MicrosoftAccountCommand.ts
+++ src/commands/MicrosoftAccountCommand.ts
@@ -23,9 +23,9 @@ class MicrosoftAccountCommand extends BaseCommand {
 
 As of the 1st of December, all new purchases of Minecraft Java Edition will require the use of a Microsoft account. Migration of old accounts will happen in 2021.
 
-New purchases and migrated accounts will be unable to use ATLauncher and some other third party tools for Minecraft that require authentication with the new Microsoft accounts.
+**New purchases and migrated accounts will be unable to use ATLauncher** and some other third party tools for Minecraft that require authentication with the new Microsoft accounts.
 
-Mojang have stated that they will add support for third party tools, but they have yet to provide any way for third party tools to use Microsoft accounts and currently they have not communicated any news about when it will be coming. W're fully at Microsoft's mercy unfortunately.`,
+Mojang have stated that they will add support for third party tools, but they have yet to provide any way for third party tools to use Microsoft accounts and currently they have not communicated any news about when it will be coming. We're fully at Microsoft's mercy unfortunately.`,
         );
 
         message.delete();
