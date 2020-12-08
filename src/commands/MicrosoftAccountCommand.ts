1f13ac2ddf367930b4cf4de38c248337b731fbcf
Ryan Dowling <ryan@ryandowling.me>
2020-12-09 08:33:38 +1100

    feat: add in GitHub issue to !microsoftaccount

--- src/commands/MicrosoftAccountCommand.ts
+++ src/commands/MicrosoftAccountCommand.ts
@@ -25,7 +25,9 @@ As of the 1st of December, all new purchases of Minecraft Java Edition will requ
 
 **New purchases and migrated accounts will be unable to use ATLauncher** and some other third party tools for Minecraft that require authentication with the new Microsoft accounts.
 
-Mojang have stated that they will add support for third party tools, but they have yet to provide any way for third party tools to use Microsoft accounts and currently they have not communicated any news about when it will be coming. We're fully at Microsoft's mercy unfortunately.`,
+Mojang have stated that they will add support for third party tools, but they have yet to provide any way for third party tools to use Microsoft accounts and currently they have not communicated any news about when it will be coming. We're fully at Microsoft's mercy unfortunately.
+
+To stay up to date on our support for Microsoft accounts, take a look at https://github.com/ATLauncher/ATLauncher/issues/415 which contains our progress towards supporting them. You can sign up for notifications on that GitHub issue to know when we release support.`,
         );
 
         message.delete();
