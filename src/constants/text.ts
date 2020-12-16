41b7373da1518d0b9f17308fc9632a6e026d168c
Ryan Dowling <ryan@ryandowling.me>
2020-12-16 21:10:18 +1100

    feat: remove microsoft account commands/watchers

--- src/constants/text.ts
+++
@@ -1,9 +0,0 @@
-export const MICROSOFT_ACCOUNT_BLURB = `Mojang have announced that Java edition will require linking a Microsoft account (https://www.minecraft.net/en-us/article/java-edition-moving-house) in order to use in the future.
-
-As of the 1st of December, all new purchases of Minecraft Java Edition will require the use of a Microsoft account. Migration of old accounts will happen in 2021.
-
-**New purchases and migrated accounts will be unable to use ATLauncher** and some other third party tools for Minecraft that require authentication with the new Microsoft accounts.
-
-Mojang have stated that they will add support for third party tools, but they have yet to provide any way for third party tools to use Microsoft accounts and currently they have not communicated any news about when it will be coming. We're fully at Microsoft's mercy unfortunately.
-
-To stay up to date on our support for Microsoft accounts, take a look at https://github.com/ATLauncher/ATLauncher/issues/415 which contains our progress towards supporting them. You can sign up for notifications on that GitHub issue to know when we release support.`;
