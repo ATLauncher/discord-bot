73e7165f79124a621f374fd73f1ecb1cab4a0065
Ryan Dowling <ryan@ryandowling.me>
2021-01-06 10:54:00 +1100

    feat: add in !help debugbat command for Debug.bat debugging

--- src/commands/HowCommand.ts
+++ src/commands/HowCommand.ts
@@ -19,7 +19,8 @@ class HowCommand extends BaseCommand {
     /**
      * The description of what the command does.
      */
-    description = 'This will post a message with how to do something. Currently only works with `!how portforward`.';
+    description =
+        'This will post a message with how to do something. Currently only works with `!how portforward` and `!how debugbat`.';
 
     topics: Topic[] = [
         {
@@ -27,6 +28,12 @@ class HowCommand extends BaseCommand {
             title: 'How to port forward',
             description:
                 "Port forwarding is the process of forwarding ports coming in to your IP address and routing them through your router to a specific computer on your network.\n\nThis is necessary in order to run Minecraft server's from your own home network.\n\nThe process for doing this will vary depending on your router:\n\n1. Find the brand and model of your router.\n2. Visit [https://portforward.com/router.htm](https://portforward.com/router.htm) and find your routers brand (Note this site shows ads which say you need to pay, **you do NOT need to pay anything**, you can close the ads.\n3. Find the model number of your modem on the brands page. Sometimes it may not specifically list your modem, find one that's similar if yours isn't listed.\n4. Follow the instructions listed on that page for Minecraft which uses port **25565** on both **UDP** and **TCP**.\n5. Others should now be able to connect with your external IP which can be found on [https://myexternalip.com/](https://myexternalip.com/).\n\nIf you are struggling to do this or it's not working, speak with your ISP's support/helpdesk. Sometimes they may be blocking it from working.",
+        },
+        {
+            command: 'debugbat',
+            title: 'Debugging ATLauncher not starting',
+            description:
+                'Download [this file](https://cdn.atlcdn.net/Debug.bat) to the same folder that ATLauncher is in, then double click it to run it and leave it for a minute and it should then create and open a Debug.txt file in the same folder. Copy paste that into https://pastebin.com/ and then share the link.',
         },
     ];
 
