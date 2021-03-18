895bb8c9a9711e0943e6e474499dcf3e1570999f
Ryan Dowling <ryan@ryandowling.me>
2021-03-19 05:20:35 +1100

    fix: emoji reaction watchers

--- src/watchers/LogsPlzReactionWatcher.ts
+++ src/watchers/LogsPlzReactionWatcher.ts
@@ -16,32 +16,33 @@ class LogsPlzReactionWatcher extends BaseWatcher {
         const reaction = args[0];
         const user = args[1];
 
-        if (
-            reaction.emoji.id === config.get<string>('reactionEmoji.logsPlz') &&
-            (!reaction.message.member?.roles.cache.has(config.get<string>('roles.moderators')) ||
+        if (reaction.emoji.id === config.get<string>('reactionEmoji.logsPlz')) {
+            if (
+                reaction.message.member?.roles.cache.has(config.get<string>('roles.moderators')) ||
                 reaction.message.member?.roles.cache.has(config.get<string>('roles.helpers')) ||
-                reaction.message.member?.roles.cache.has(config.get<string>('roles.packDeveloper')))
-        ) {
-            await reaction.remove();
+                reaction.message.member?.roles.cache.has(config.get<string>('roles.packDeveloper'))
+            ) {
+                const sentMessage = await reaction.message.reply(
+                    `In order to help you, we need some logs. Please see ` +
+                        'https://cdn.atlcdn.net/UploadLogs.gif on how to generate the link. Please make sure that you ' +
+                        'press the button after the error/issue occurs.  Once done please paste the link here. If the logs ' +
+                        "don't upload or this is an issue with a server, please upload your logs to https://paste.ee/ " +
+                        'and give us the link.',
+                );
 
-            const sentMessage = await reaction.message.reply(
-                `In order to help you, we need some logs. Please see ` +
-                    'https://cdn.atlcdn.net/UploadLogs.gif on how to generate the link. Please make sure that you ' +
-                    'press the button after the error/issue occurs.  Once done please paste the link here. If the logs ' +
-                    "don't upload or this is an issue with a server, please upload your logs to https://paste.ee/ " +
-                    'and give us the link.',
-            );
+                if (reaction.message.channel.type !== 'dm') {
+                    // delete message after 24 hours
+                    sentMessage.delete({
+                        timeout: 60 * 60 * 24 * 1000,
+                    });
 
-            if (reaction.message.channel.type !== 'dm') {
-                // delete message after 24 hours
-                sentMessage.delete({
-                    timeout: 60 * 60 * 24 * 1000,
-                });
+                    await sentMessage.react('🇱');
+                    await sentMessage.react('🇴');
+                    await sentMessage.react('🇬');
+                    await sentMessage.react('🇸');
+                }
 
-                await sentMessage.react('🇱');
-                await sentMessage.react('🇴');
-                await sentMessage.react('🇬');
-                await sentMessage.react('🇸');
+                await reaction.remove();
             }
         }
     }
--- src/watchers/UseMinecraftSupportReactionWatcher.ts
+++ src/watchers/UseMinecraftSupportReactionWatcher.ts
@@ -15,33 +15,34 @@ class UseMinecraftSupportReactionWatcher extends BaseWatcher {
     async action(method: keyof Discord.ClientEvents, ...args: Discord.ClientEvents['messageReactionAdd']) {
         const reaction = args[0];
 
-        if (
-            reaction.emoji.id === config.get<string>('reactionEmoji.useMinecraftSupport') &&
-            (!reaction.message.member?.roles.cache.has(config.get<string>('roles.moderators')) ||
+        if (reaction.emoji.id === config.get<string>('reactionEmoji.useMinecraftSupport')) {
+            if (
+                reaction.message.member?.roles.cache.has(config.get<string>('roles.moderators')) ||
                 reaction.message.member?.roles.cache.has(config.get<string>('roles.helpers')) ||
-                reaction.message.member?.roles.cache.has(config.get<string>('roles.packDeveloper')))
-        ) {
-            const minecraftSupportChannel = this.bot.client.channels.cache.find(
-                ({ id }) => id === config.get<string>('channels.minecraftSupport'),
-            ) as Discord.TextChannel;
+                reaction.message.member?.roles.cache.has(config.get<string>('roles.packDeveloper'))
+            ) {
+                const minecraftSupportChannel = this.bot.client.channels.cache.find(
+                    ({ id }) => id === config.get<string>('channels.minecraftSupport'),
+                ) as Discord.TextChannel;
 
-            const sentMessage = await reaction.message.reply(
-                `Your message has been deleted as it's not in the correct channel. Issues with Minecraft itself, after the Play button is pressed, should be posted in ${minecraftSupportChannel}. Please repost your message, along with your logs, to that channel.`,
-            );
+                const sentMessage = await reaction.message.reply(
+                    `Your message has been deleted as it's not in the correct channel. Issues with Minecraft itself, after the Play button is pressed, should be posted in ${minecraftSupportChannel}. Please repost your message, along with your logs, to that channel.`,
+                );
 
-            if (reaction.message.channel.type !== 'dm') {
-                // delete message after 24 hours
-                sentMessage.delete({
-                    timeout: 60 * 60 * 24 * 1000,
-                });
+                if (reaction.message.channel.type !== 'dm') {
+                    // delete message after 24 hours
+                    sentMessage.delete({
+                        timeout: 60 * 60 * 24 * 1000,
+                    });
+                }
+
+                if (reaction.message.deletable) {
+                    await reaction.message.delete();
+                }
             }
 
-            if (reaction.message.deletable) {
-                await reaction.message.delete();
-            }
+            await reaction.remove();
         }
-
-        await reaction.remove();
     }
 }
 
