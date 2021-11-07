cf5678dc3e76c9c14b37ae06ce92052f6f55354b
Ryan Dowling <ryan@ryandowling.me>
2021-11-07 18:11:52 +1100

    feat: add in thread react watcher to make new threads and add the reactor to new threads

--- src/watchers/StartThreadReactionWatcher.ts
+++ src/watchers/StartThreadReactionWatcher.ts
@@ -3,7 +3,7 @@ import * as Discord from 'discord.js';
 
 import BaseWatcher from './BaseWatcher';
 
-class LogsPlzReactionWatcher extends BaseWatcher {
+class StartThreadReactionWatcher extends BaseWatcher {
     /**
      * The method this watcher should listen on.
      */
@@ -16,7 +16,7 @@ class LogsPlzReactionWatcher extends BaseWatcher {
         const reaction = args[0];
         const reactingUser = args[1];
 
-        if (reaction.emoji.id === config.get<string>('reactionEmoji.logsPlz')) {
+        if (reaction.emoji.name === '🧵') {
             await reaction.remove();
 
             const reactingMember = this.bot.client.guilds.cache
@@ -28,23 +28,13 @@ class LogsPlzReactionWatcher extends BaseWatcher {
                 reactingMember?.roles.cache.has(config.get<string>('roles.helpers')) ||
                 reactingMember?.roles.cache.has(config.get<string>('roles.packDeveloper'))
             ) {
-                const messageReply =
-                    `In order to help you, we need some logs. Please see ` +
-                    'https://cdn.atlcdn.net/UploadLogs.gif on how to generate the link. Please make sure that you ' +
-                    'press the button after the error/issue occurs. Once done please paste the link here. If the logs ' +
-                    "don't upload or this is an issue with a server, please upload your logs to https://paste.ee/ " +
-                    'and give us the link.';
-                let sentMessage;
-
                 // if we're not in a thread, then start one
                 if (!reaction.message.channel.isThread()) {
                     const isFromBot = reaction.message.author?.bot;
-                    const memberName = isFromBot
-                        ? reaction.message.mentions.users.first()?.username
-                        : reaction.message.author?.username;
+                    const user = isFromBot ? reaction.message.mentions.users.first() : reaction.message.author;
 
                     const thread = await reaction.message.startThread({
-                        name: `${memberName}'s Issue`,
+                        name: `${user?.username}'s Thread`,
                         autoArchiveDuration: 1440, // 1 day
                     });
 
@@ -54,20 +44,13 @@ class LogsPlzReactionWatcher extends BaseWatcher {
                         thread.send(`${reaction.message.mentions.users.first()}`);
                     }
 
-                    sentMessage = await thread.send(messageReply);
-                } else {
-                    sentMessage = await reaction.message.reply(messageReply);
-                }
-
-                if (sentMessage && sentMessage.deletable) {
-                    await sentMessage.react('🇱');
-                    await sentMessage.react('🇴');
-                    await sentMessage.react('🇬');
-                    await sentMessage.react('🇸');
+                    await thread.send(
+                        `A thread has been spun up for you ${user}. Please keep any relevant messages in here to keep things organised and tidy.`,
+                    );
                 }
             }
         }
     }
 }
 
-export default LogsPlzReactionWatcher;
+export default StartThreadReactionWatcher;
