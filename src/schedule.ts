61b35c9f9913266a7c6a9ed6f26d0ecd3273dfc5
Ryan Dowling <ryan@ryandowling.me>
2022-04-21 17:44:20 +1000

    feat: auto archive unused support threads after 1 hour of no response

--- src/schedule.ts
+++ src/schedule.ts
@@ -4,6 +4,7 @@ import schedule from 'node-schedule';
 import type Bot from './Bot';
 import prisma from './utils/prisma';
 import logger from './utils/logger';
+import { isAfter, subHours } from 'date-fns';
 
 const removeJailedUsers = async (bot: Bot) => {
     logger.debug('Scheduler::removeJailedUsers - running');
@@ -35,8 +36,35 @@ const removeJailedUsers = async (bot: Bot) => {
     }
 };
 
+const archiveUnusedSupportThreads = async (bot: Bot) => {
+    logger.debug('Scheduler::archiveUnusedSupportThreads - running');
+
+    const minecraftSupportChannel = bot.getMinecraftSupportChannel();
+    if (minecraftSupportChannel) {
+        const threads = await minecraftSupportChannel.threads.fetchActive(false);
+        threads.threads
+            .filter((t) => t.messageCount === 1 && isAfter(subHours(new Date(), 1), t.createdAt))
+            .forEach((t) => {
+                logger.debug(`Archiving thread named ${t.name} as there are no messages after 1 hour.`);
+                t.setArchived(true);
+            });
+    }
+
+    const launcherSupportChannel = bot.getLauncherSupportChannel();
+    if (launcherSupportChannel) {
+        const threads = await launcherSupportChannel.threads.fetchActive(false);
+        threads.threads
+            .filter((t) => t.messageCount === 1 && isAfter(subHours(new Date(), 1), t.createdAt))
+            .forEach((t) => {
+                logger.debug(`Archiving thread named ${t.name} as there are no messages after 1 hour.`);
+                t.setArchived(true);
+            });
+    }
+};
+
 export const startScheduler = (bot: Bot) => {
     logger.info('Scheduler started');
 
     schedule.scheduleJob('* * * * *', () => removeJailedUsers(bot));
+    schedule.scheduleJob('*/10 * * * *', () => archiveUnusedSupportThreads(bot));
 };
